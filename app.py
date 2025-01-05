da643017587b1e7320b246e74a86c9710c0a590e
NitinBot001 <112721096+NitinBot001@users.noreply.github.com>
2025-01-05 08:42:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+
+CORS(app)
 
 # Replace with your API key
 API_KEY = 'AIzaSyBQBfbhPqtL2SEKh3l_0cE6zIxwMKLTi-A'
